b728931465b1b06d47e78880e1babffad80a5719
andela-eashikodi <emeka.ashikodi@andela.co>
2015-09-18 19:34:47 +0100

    require express config

---
+++ server.js
@@ -0,0 +1,10 @@
+var app = require('./config/express')();
+
+app.listen('7000', function(err) {
+  if(err) {
+    throw err;
+  }
+  console.log('Server started on port 7000');
+});
+
+module.exports = app;
