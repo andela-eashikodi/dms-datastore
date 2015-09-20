4624d5c19a1e5934b5dc8c5133a52f8706ae75c8
andela-eashikodi <emeka.ashikodi@andela.co>
2015-09-20 15:32:04 +0100

    add user authentication function

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,9 +1,66 @@
 require('../models/user.schema');
 require('../models/document.schema');
+var jwt = require('jsonwebtoken');
+var config = require('../../config/config');
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 var Document = mongoose.model('Document');
 var UserController = function() {};
+
+UserController.prototype.userAuth = function(req, res) {
+  User.findOne({email: req.body.email}, function(err, user) {
+    if (!user) {
+      return res.json({
+        success: false,
+        message: 'Failed to authenticate user.'
+      });
+    } 
+    else {
+      var validPassword = user.comparePassword(req.body.password);
+      if (validPassword) {
+        var token = jwt.sign(user, config.secret, {
+          expiresInMinutes: 1440 //24hr expiration
+        });
+        res.json({
+          success: true,
+          token: token
+        });
+      } 
+      else {
+        return res.json({
+          success: false,
+          message: 'Failed to authenticate user.'
+        });
+      }
+    }
+  });
+};
+
+UserController.prototype.verifyUserAuth = function(req, res, next) {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  if (token) {
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Failed to authenticate token.'
+        });
+      } else {
+        //if all checks are passed, save decoded info to request
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    //show http 403 message when token is not provided
+    return res.json({
+      success: false,
+      message: 'No token provided.'
+    });
+
+  }
+};
 
 UserController.prototype.verifyUser = function(req, res, next) {
    User.findOne({email: req.body.email}, function(err, user) {
@@ -32,17 +89,17 @@ UserController.prototype.verifyUser = function(req, res, next) {
 
 UserController.prototype.userLogin = function(req, res) {
   if(req.session && req.session.user) {
+    /*
+    most appropriate to redirect user
+    like this 
+    res.redirect('/dashboard')
+    */
     res.json({
       success: true,
       message: 'User login succesful'
     });
   }
   else {
-    /*
-    most appropriate to redirect user
-    like this 
-    res.redirect('/')
-    */
     res.json({
       success: false,
       message: 'User login failed'
@@ -52,6 +109,11 @@ UserController.prototype.userLogin = function(req, res) {
 
 UserController.prototype.userLogout = function(req, res) {
   req.session.reset();
+  /*
+    most appropriate to redirect user
+    like this 
+    res.redirect('/login-page')
+    */
   res.json({
     success: true,
     message: 'User Logout succesful'
