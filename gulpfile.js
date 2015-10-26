99fa92ac801c3af241d9c96e640744ce3190f578
andela-eashikodi <emeka.ashikodi@andela.co>
2015-10-27 00:59:11 +0100

    change files for minification

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,16 @@ var gulp  = require('gulp'),
 
 var staticFiles = {
   js: [
-    'public/libs/angular/angular.min.js',
-    'public/libs/angular-animate/angular-animate.min.js',
-    'public/libs/angular-aria/angular-aria.min.js',
-    'public/libs/angular-material/angular-material.min.js',
-    'public/libs/angular-messages/angular-messages.min.js',
-    'public/libs/angular-resource/angular-resource.min.js',
+    'public/libs/angular/angular.js',
+    'public/libs/angular-animate/angular-animate.js',
+    'public/libs/angular-aria/angular-aria.js',
+    'public/libs/angular-material/angular-material.js',
+    'public/libs/angular-messages/angular-messages.js',
+    'public/libs/angular-resource/angular-resource.js',
     'public/libs/ngstorage/ngStorage.js',
-    'public/libs/angular-ui-router/release/angular-ui-router.min.js',
-    'public/libs/angular-material-icons/angular-material-icons.min.js'
+    'public/libs/angular-ui-router/release/angular-ui-router.js',
+    'public/libs/angular-material-icons/angular-material-icons.js',
+    'public/libs/angularUtils-pagination/dirPagination.js'
   ]
 };
 
