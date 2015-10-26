65c662fabaa03ba3f659dd4d6680c9b2a677012e
andela-eashikodi <emeka.ashikodi@andela.co>
2015-10-27 00:46:55 +0100

    add params for pagination

--- public/app/controllers/home.controller.js
+++ public/app/controllers/home.controller.js
@@ -2,6 +2,10 @@
 
 angular.module('DmsApp').controller('homeCtrl', ['$scope', '$rootScope', '$location', '$localStorage', '$window', '$timeout', 'sideNav', 'DocumentService', '$resource', function($scope, $rootScope, $location, $localStorage, $window, $timeout, sideNav, DocumentService, $resource) {
 
+  // parameters for pagination module
+  $scope.currentPage = 1;
+  $scope.pageSize = 10;
+
   //$scope.close used to close nav bar upon click
   $scope.close = sideNav.close;
 
